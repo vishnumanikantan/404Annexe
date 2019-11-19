fbc8ee6b595afb9b1d3a25b6cbf313bcb3c12f57
vishubvinod0602 <vishubvinod0602@gmail.com>
2019-11-19 20:22:16 +0530

    footer update

--- js/main.js
+++ js/main.js
@@ -331,3 +331,16 @@ jQuery(document).ready(function($) {
 
 });
 
+$('.carousel.item').each(function(){
+	var next = $(this).next();
+	if (!next.length) {
+	  next = $(this).siblings(':first');
+	}
+	next.children(':first-child').clone().appendTo($(this));
+	
+	if (next.next().length>0) {
+	  next.next().children(':first-child').clone().appendTo($(this));
+	} else {
+		$(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+	}
+  });
