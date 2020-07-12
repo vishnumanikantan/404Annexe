a364dc7c900d451c11ffc0bace07e98520eb8f42
vishnubvinod0602 <vishnubvinod0602@gmail.com>
2020-07-13 04:05:59 +0530

    first commit

--- js/main.js
+++ js/main.js
@@ -331,42 +331,29 @@ jQuery(document).ready(function($) {
 
 });
 
-$('#carousel-1').on('slide.bs.carousel', function (e) {
-    var $e = $(e.relatedTarget);
-    var idx = $e.index();
-    var itemsPerSlide = 12;
-    var totalItems = $('.carousel-item').length;
- 
-    if (idx >= totalItems-(itemsPerSlide-1)) {
-        var it = itemsPerSlide - (totalItems - idx);
-        for (var i=0; i<it; i++) {
-            // append slides to end
-            if (e.direction=="left") {
-                $('.carousel-item').eq(i).appendTo('.carousel-inner');
-            }
-            else {
-                $('.carousel-item').eq(0).appendTo('.carousel-inner');
-            }
-        }
-    }
-});
+// JavaScript Document
 
-$('#carousel-2').on('slide.bs.carousel', function (e) {
-    var $e = $(e.relatedTarget);
-    var idx = $e.index();
-    var itemsPerSlide = 12;
-    var totalItems = $('.carousel-item').length;
- 
+$('#web').on('slide.bs.carousel', modCarousel);
+$('#junior').on('slide.bs.carousel', modCarousel);
+
+function modCarousel (e) {
+
+    var $e = $(e.relatedTarget),
+        idx = $e.index(),
+        itemsPerSlide = 3,
+        $c = $e.closest('.carousel-inner'),
+        totalItems = $('.carousel-item', $c).length;
+    
     if (idx >= totalItems-(itemsPerSlide-1)) {
         var it = itemsPerSlide - (totalItems - idx);
         for (var i=0; i<it; i++) {
             // append slides to end
             if (e.direction=="left") {
-                $('.carousel-item').eq(i).appendTo('.carousel-inner');
+                $('.carousel-item', $c).eq(i).appendTo($c);
             }
             else {
-                $('.carousel-item').eq(0).appendTo('.carousel-inner');
+                $('.carousel-item', $c).eq(0).appendTo($c);
             }
         }
     }
-});
+};
